39a643f4ec043ef56754d672bffe37229c5fbadd
Alejandro García <92.alejandrogarcia@gmail.com>
2018-11-16 16:23:32 +0100

    operators exercise

--- ConditionalStatements/src/com/alejandro/Main.java
+++ ConditionalStatements/src/com/alejandro/Main.java
@@ -30,6 +30,19 @@ public class Main {
         System.out.println("wasCar = " + wasCar);
 
         // Challenge:
-        
+        // 1. Create a double variable with the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that display a message "Total was over the limit"
+        //      if the remainding total (#4) is equal to 20 or less.
+
+        double myDouble = 20d;
+        double mySecondDouble = 80d;
+        double result = (myDouble + mySecondDouble) * 25;
+        double remainder = result % 40;
+        System.out.println(remainder);
+        if (remainder >= 20)
+            System.out.println("Total was over the limit");
     }
 }
